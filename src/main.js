a2ba9249302099a72742a9170af37c69a6075ed3
Jacob Goh <jacobgoh101@gmail.com>
2017-07-23 23:24:11 +0800

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,23 +1,32 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex';
+import { sync } from 'vuex-router-sync';
 import App from './App'
 import router from './router'
 import GSignInButton from 'vue-google-signin-button'
 import VuePino from 'vue-pino'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import VuexStore from './vuex/store';
 
+Vue.use(Vuex)
 Vue.use(GSignInButton)
 Vue.use(VuePino)
 Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
+const store = new Vuex.Store(VuexStore);
+
+sync(store, router);
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
---
+++ src/vuex/store.js
@@ -0,0 +1,11 @@
+export default {
+  modules: {},
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    }
+  }
+};
